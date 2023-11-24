2913b214a21af38022ad1bb074d079db81340981
BhavyaCPatel <bhavyaaes@gmail.com>
2023-11-24 16:42:18 +0530

    Final Commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     return(
         <nav>
             <div className="nav-center">
-                <h3>Redux Toolkit</h3>
+                <h3>Shopping Cart</h3>
                 <div className="nav-container">
                     <CartIcon/>
                     <div className="amount-container">
